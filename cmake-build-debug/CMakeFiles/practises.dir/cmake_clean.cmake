02071f1b45a65d8882e338b8285a56e3182b9f78
Karol Kazmierczak <kazmierczakkarol1999@gmail.com>
2020-10-24 17:40:57 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/practises.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practises.dir/main.c.o"
+  "practises"
+  "practises.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/practises.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/practises.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for practises.
+# This may be replaced when dependencies are built.
