4c8675ac1b1663ec847db6b53f135002fcf32fb0
Karol Kazmierczak <kazmierczakkarol1999@gmail.com>
2020-10-27 17:12:21 +0100

    add working on null elem

--- z1.c
+++ z1.c
@@ -11,178 +11,259 @@ typedef struct elem el;
 
 el *create(int x)
 {
-    el *elem = (el *)malloc(sizeof(el));
-    elem->x = x;
-    elem->next = NULL;
-    return elem;
+    el *head = (el *)malloc(sizeof(el));
+    head->x = x;
+    head->next = NULL;
+    return head;
 }
 
-void deleteEl(el *elem)
+void deleteEl(el *head)
 {
-    el *help = elem;
+
+    el *help = head;
     while (help != NULL)
     {
-        help = elem->next;
-        free(elem);
-        elem = help;
+        help = head->next;
+        free(head);
+        head = help;
     }
 }
 
-void pritnEl(el *elem)
+void pritnEl(el *head)
 {
-    el *printer = elem;
-    while (printer != NULL)
+    // printf("%d %p \n", head->x, head);
+
+    if (head != NULL)
     {
-        printf("%d\n", printer->x);
-        printer = printer->next;
+
+        el *printer = head;
+        while (printer != NULL)
+        {
+            printf("%d\n", printer->x);
+            printer = printer->next;
+        }
+        printf("___________\n");
     }
-    printf("___________\n");
 }
 
-el *getLast(el *elem)
+el *getLast(el *head)
 {
-    el *tmp = elem;
-    while (tmp->next != NULL)
+    if (head != NULL)
     {
-        tmp = tmp->next;
+        el *tmp = head;
+        while (tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+
+        return tmp;
     }
-
-    return tmp;
+    return NULL;
 }
 
-el *addTop(el *elem, int y)
+el *addTop(el *head, int y)
 {
-    el *newElem = (el *)malloc(sizeof(el));
-    newElem->x = y;
-    newElem->next = elem;
-    return newElem;
+    el *newhead = (el *)malloc(sizeof(el));
+    newhead->x = y;
+    if (head != NULL)
+    {
+        newhead->next = head;
+    }
+    else
+    {
+        newhead->next = NULL;
+    }
+    return newhead;
 }
 
-void addBottom(el *elem, int y)
+el *addBottom(el *head, int y)
 {
-    el *bottom = (el *)malloc(sizeof(el));
-    bottom->x = y;
-    bottom->next = NULL;
+    if (head != NULL)
+    {
+        el *bottom = (el *)malloc(sizeof(el));
+        bottom->x = y;
+        bottom->next = NULL;
 
-    el *last = getLast(elem);
-    last->next = bottom;
+        el *last = getLast(head);
+        last->next = bottom;
+    }
+    else
+    {
+        head = create(y);
+    }
+    return head;
 }
 
 el *find(el *head, int x)
 {
-    el *help = head;
-    int i = 0;
-    while (help->x != x && help->next != NULL)
+    if (head != NULL)
     {
-        help = help->next;
-        i++;
-    }
-    if (help->x == x)
-    {
-        printf("%d found on %d position\n", x, i);
-        return help;
+        el *help = head;
+        int i = 0;
+        while (help->x != x && help->next != NULL)
+        {
+            help = help->next;
+            i++;
+        }
+        if (help->x == x)
+        {
+            printf("%d found on %d position\n", x, i);
+            return help;
+        }
+        else
+        {
+            printf("%d not found\n", x);
+            return head;
+        }
     }
     else
     {
-        printf("%d not found\n", x);
-        return head;
+        printf("list is empty\n");
+        return NULL;
     }
 }
 
 void addAfter(el *target, int y)
 {
-    el *newEl = (el *)malloc(sizeof(el));
-    newEl->x = y;
-    newEl->next = target->next;
-    target->next = newEl;
+    if (target != NULL)
+    {
+        el *newEl = (el *)malloc(sizeof(el));
+        newEl->x = y;
+        newEl->next = target->next;
+        target->next = newEl;
+    }
 }
 
 void addBefore(el *head, el *target, int y)
 {
-    el *help = head;
-    while (help->next != target && help->next != NULL)
+    if (head != NULL)
     {
-        help = help->next;
+        el *help = head;
+        while (help->next != target && help->next != NULL)
+        {
+            help = help->next;
+        }
+        el *newEl = (el *)malloc(sizeof(el));
+        newEl->x = y;
+        newEl->next = target;
+        help->next = newEl;
     }
-    el *newEl = (el *)malloc(sizeof(el));
-    newEl->x = y;
-    newEl->next = target;
-    help->next = newEl;
 }
 
 el *pop(el *head)
 {
-    el *newHead = head->next;
-    free(head);
-    return newHead;
-}
-
-void drop(el *head)
-{
-    el *help = head;
-    el *before = NULL;
-    while (help->next != NULL)
+    if (head != NULL)
     {
-        before = help;
-        help = help->next;
+        el *newHead = NULL;
+        if (head->next != NULL)
+        {
+            newHead = head->next;
+        }
+        free(head);
+        return newHead;
     }
-    free(help);
-    if (before != NULL)
+    else
     {
-        before->next = NULL;
+        return NULL;
     }
 }
 
-el* removeEl(el *head, el *toRemove)
+el *drop(el *head)
 {
-    el *help = head;
-    el *before = NULL;
-    while (help != toRemove && help->next != NULL)
+    if (head != NULL)
     {
-        before = help;
-        help = help->next;
+        el *help = head;
+        el *before = NULL;
+        while (help->next != NULL)
+        {
+            before = help;
+            help = help->next;
+        }
+        el* returnData = head;
+        if(help == head){
+            returnData = NULL;
+        }
+        free(help);
+        if (before != NULL)
+        {
+            before->next = NULL;
+        }
+        return returnData;
     }
-    if(before != NULL){
-        before->next = help->next;
+    return NULL;
+
+}
+
+el *removeEl(el *head, el *toRemove)
+{
+    if (head != NULL)
+    {
+        el *help = head;
+        el *before = NULL;
+        while (help != toRemove && help->next != NULL)
+        {
+            before = help;
+            help = help->next;
+        }
+        if (before != NULL)
+        {
+            before->next = help->next;
+        }
+        el *returnVaule = head;
+        if (head == toRemove)
+        {
+            if (head->next != NULL)
+            {
+                returnVaule = head->next;
+            }
+            else
+            {
+                returnVaule = NULL;
+            }
+        }
+        free(help);
+        return returnVaule;
     }
-    el *returnVaule = head;
-    if(head == toRemove){
-        returnVaule = head->next;
+    else
+    {
+        return NULL;
     }
-    free(help);
-    return returnVaule;
 }
 
 int main(int argc, char const *argv[])
 {
-    el *head = create(5);
-    // el *head;
-    pritnEl(head);
-    head = addTop(head, 6);
-    pritnEl(head);
-    addBottom(head, 9);
-    head = addTop(head, 29);
+    // el *head = create(5);
+    el *head = NULL;
+    // pritnEl(head);
+    // head = addTop(head, 6);
+    // pritnEl(head);
+
+    head = addBottom(head, 9);
+    // head = addTop(head, 29);
+    // addBottom(head, 9);
+
+    // pritnEl(head);
+
+    // el *fd = find(head, 9);
+    // fd = find(head, 234);
+    // printf("___________\n");
     pritnEl(head);
 
-    el *fd = find(head, 9);
-    fd = find(head, 234);
-    printf("___________\n");
-    pritnEl(head);
+    // addAfter(head, 10);
+    // addBefore(head, head, 199);
+    printf("_______________\n");
 
-    addAfter(head->next, 10);
-    addBefore(head, head->next, 199);
+    // head = pop(head);
     pritnEl(head);
+    head = drop(head);
+    // pritnEl(head);
 
-    head = pop(head);
-    drop(head);
-    pritnEl(head);
-    
-    head = removeEl(head, head->next);
-    pritnEl(head);
+    // head = removeEl(head, head->next);
+    // pritnEl(head);
 
-    head = removeEl(head, head);
+    // head = removeEl(head, head);
 
-    pritnEl(head);
+    // pritnEl(head);
 
     deleteEl(head);
     return 0;
